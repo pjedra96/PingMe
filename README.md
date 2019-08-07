e066eba4bf65404c0805bd6cf27d3cad4f6ec009
pjedra96 <peterj1124@hotmail.com>
2019-08-07 15:04:11 +0100

    version 1.0
    - initial version with emojis

---
+++ README.md
@@ -0,0 +1,9 @@
+# Description
+This application is a Nodejs-based real-time chat application with incorporated emojis (uses Emoji-Picker-jQuery-Emoji-Plugin).
+
+# Getting Started
+1. Clone the repository (https://www.github.com/pjedra96/real-time-chat) to a directory of your choice.
+2. Ensure that you have NodeJS installed (v6+) on your system. 
+3. Install all dependencies on your machine at the root of the repository clone, using `npm install`.
+4. Execute `npm start` from the NodeJS Command Prompt at the root of the clone.
+5. `localhost:8080` in the browser.
---
+++ index.js
@@ -0,0 +1,49 @@
+// Module Dependencies
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const path = require('path');
+
+// Sets /views as the default directory
+app.use(express.static(path.join(__dirname, 'views'))); 
+
+// Setting / as the main route of the application, which serves the chat page
+app.get('/', function(req, res) {
+    res.render('index.ejs');
+});
+// Socket.io configuration - event listeners
+io.sockets.on('connection', function(socket) {
+    // Listens to the events emitted by the clients and broadcasts them to all clients
+    socket.on('username', function(username) {
+        socket.username = username;
+        io.emit('is_online', '<span id="joined"></span> <i><strong>' + socket.username + '</strong> joined the chat..</i>'); // 🔵
+    });
+
+    socket.on('disconnect', function(username) {
+        io.emit('is_online', '<span id="left"></span> <i><strong>' + socket.username + '</strong> left the chat..</i>'); // 🔴
+    })
+
+    socket.on('chat_message', function(message) {
+        io.emit('chat_message', '<strong>' + socket.username + '</strong>: ' + message);
+    });
+
+    socket.on('typing', function() {
+        io.emit('typing', { notification: socket.username + " is typing", username: socket.username});
+    });
+
+    socket.on('doneTyping', function() {
+        io.emit('doneTyping');
+    });
+
+});
+
+const server = http.listen(8080, function() {
+    console.log('listening on *:8080');
+});
+
+// Stops the entire process when Signal Interrupt (Control-C) issued in the console
+process.on('SIGINT', function(){
+  console.log('Signal Interrupt received. Exiting the process');
+  process.exit(0);
+});
